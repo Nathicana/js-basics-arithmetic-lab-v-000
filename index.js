da8e65f8e98f47c7a168515dcd86f60c985ec15d
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-17 04:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,4 @@ const ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random()*20);
 
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = Math.floor(Math.random(randomInteger)*1000000020;
